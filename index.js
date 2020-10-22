6443ca488f3cd5671871051e046f898088a79dbf
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-22 18:40:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function wakeDog(dogName, dogBreed) {
-  let action = console.log(`Wake ${dogName} the ${dogBreed}`);
+  return (`Wake ${dogName} the ${dogBreed}`);
 }
 function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
